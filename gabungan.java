69335ac62d3f1593e55b27062e8cecc68e040034
Rendha <rendhazuper@gmail.com>
2023-12-03 07:38:59 +0700

    baru

--- gabungan.java
+++ gabungan.java
@@ -1,8 +1,55 @@
 import java.util.*;
 public class gabungan {
+static Scanner key = new Scanner(System.in);
+static int[][] kamar = new int[2][];
+static String[] tipeKasur  = new String[3];
+static String[] ketersediaan = new String[2];
+static int[] hargahari = new int[2];
+static int[] hargajam = new int[2];
+
+
+//persiapan array untuk format data input tamu 
+static String [] Nama = new String[20];
+static String[] nomorHP = new String[20];
+static String[] checkin = new String[20];
+static String[] checkout = new String[20];
+
+//pengisian data array
+static{ 
+    kamar[0][0] = 101; 
+    kamar[0][1] = 102;
+    kamar[0][2] = 103;
+    kamar[0][3] = 104;
+    kamar[0][4] = 105;
+    kamar[1][0] = 201;
+    kamar[1][1] = 202;
+    kamar[1][2] = 203;
+    kamar[1][3] = 204;
+    kamar[1][4] = 205;
+    kamar[1][5] = 206;
+    kamar[1][6] = 207;
+    kamar[1][7] = 208;
+    kamar[1][8] = 209;
+    kamar[1][9] = 210;
+
+    tipeKasur[1] = "single bed";
+    tipeKasur[2] = "double bed";
+
+    ketersediaan[0] = "tersedia";
+    ketersediaan[1] = "tidak tersedia";
+
+    hargahari[0] = 145000;
+    hargahari[1] = 200000;
+
+    hargajam[0] = 50000;
+    hargajam[1] = 75000;
+
+}
+
+
 public static void main(String[] args) {
 //login terlebih dahulu
-Scanner key = new Scanner(System.in);
+
 String username, password;
 boolean sesi = false;
 int kesempatan = 3;
@@ -29,150 +76,30 @@ while (sesi) {
         System.out.println("==============================");
         System.out.println("|| 1.Booking kamar          ||");
         System.out.println("|| 2.Manage kamar           ||");
-        System.out.println("|| 3.Manage tamu            ||");
-        System.out.println("|| 4.Riwayat Tamu           ||");
+        System.out.println("|| 3.Riwayat Tamu           ||");
         System.out.println("==============================");
         System.out.print("Pilih menu : ");
         pilih_menu = key.nextInt();
-
         switch (pilih_menu) {
             case 1:
-        // Harga kamar per hari atau per jam
-        int hargaHari= 5000, hargaJam = 2000, nomorKamar,jumlah;
-        double totalHarga; 
-        String namaTamu;
-
-        // Input nama tamu
-        System.out.print("Masukkan nama tamu: ");
-        namaTamu = key.next();
-        // Input nomor kamar yang dipesan
-        System.out.print("Masukkan nomor kamar: ");
-        nomorKamar = key.nextInt();
-
-        // Tampilkan pilihan menginap hari atau jam
-        System.out.println("Pilihan menginap:");
-        System.out.println("1. Menginap Hari");
-        System.out.println("2. Menginap Jam");
-        System.out.print("Pilih opsi (1/2): ");
-        int pilihan = key.nextInt();
-
-        jumlah = 0;
-        totalHarga = 0;
-
-        if (pilihan == 1) {
-            // Pilihan pertama (hari)
-            System.out.print("Masukkan jumlah hari: ");
-            jumlah = key.nextInt();
-            totalHarga = hargaHari * jumlah;
-        } else if (pilihan == 2) {
-            // Pilihan kedua (jam)
-            System.out.print("Masukkan jumlah jam: ");
-            jumlah = key.nextInt();
-            totalHarga = hargaJam * jumlah;
-        }
-        // Input pembayaran
-        System.out.println("===============================");
-        System.out.printf("%d\t", totalHarga );
-        System.out.println("===============================");
-        System.out.println("lanjutkan pembayaran ?");
-        System.out.println("1 Ya");
-        System.out.println("2 Tidak");
-        System.out.print("Masukkan jumlah pembayaran: ");
-        double pembayaran = key.nextDouble();
-
-        System.out.print("Input tanggal masuk (hh-bb-yy)");
-        String tanggal = key.nextLine();
-
-        // Proses pembayaran dan kembalian
-        if (pembayaran > totalHarga) {
-            double kembalian = pembayaran - totalHarga;
-            System.out.println("Nama Tamu: " + namaTamu);
-            System.out.println("Nomor Kamar: " + nomorKamar);
-            System.out.println("Tanggal Booking: " + tanggal);
-            System.out.println("Total Harga: " + totalHarga);
-            System.out.println("Pembayaran : "+ pembayaran);
-            System.out.println("Kembalian: " + kembalian);
-            System.out.println("Status: Lunas");
+            System.out.println("");
+                break;
             
-        } else if (pembayaran < totalHarga) {
-            System.out.println("Pembayaran kurang. Transaksi dibatalkan.");
-             System.out.println("Nama Tamu: " + namaTamu);
-            System.out.println("Nomor Kamar: " + nomorKamar);
-            System.out.println("Tanggal Booking: " + tanggal);
-            System.out.println("Total Harga: " + totalHarga);
-            System.out.println("Status: Dibatalkan");
-        } else {
-            // Tampilkan informasi booking
-            System.out.println("Nama Tamu: " + namaTamu);
-            System.out.println("Nomor Kamar: " + nomorKamar);
-            System.out.println("Tanggal Booking: " + tanggal);
-            System.out.println("Total Harga: " + totalHarga);
-            System.out.println("Pembayaran : "+ pembayaran);
-            System.out.println("Status: Lunas");
-        }
-    
-             break;
-            case 2:   
-        // 1. Inisialisasi data
-        int[][] informasiKamar = new int[10][2]; // Misalnya, 10 kamar dengan nomor kamar dan status ketersediaan
-        // Inisialisasi ketersediaan kamar (0: tersedia, 1: tidak tersedia)
-        for (int i = 0; i < informasiKamar.length; i++) {
-            informasiKamar[i][0] = i + 1; // Nomor kamar
-            informasiKamar[i][1] = 0;    // Ketersediaan diatur menjadi tersedia awalnya
-        }
+            case 2:
 
-        // 2. Input data
-        int[][] inputKamar = new int[10][2]; // Array untuk menyimpan data input
-        for (int i = 0; i < inputKamar.length; i++) {
-            System.out.println("Masukkan informasi kamar ke-" + (i + 1));
-            System.out.print("Nomor Kamar: ");
-            inputKamar[i][0] = key.nextInt();
-
-            System.out.print("Ketersediaan (0: Tersedia, 1: Tidak Tersedia): ");
-            inputKamar[i][1] = key.nextInt();
-
-            // Mengupdate status kamar di informasiKamar
-            for (int j = 0; j < informasiKamar.length; j++) {
-                if (informasiKamar[j][0] == inputKamar[i][0]) {
-                    informasiKamar[j][1] = inputKamar[i][1];
-                    break;
-                }
-            }
-        }
-
-        // 3. Tampilkan array
-        System.out.println("\nInformasi Kamar:");
-        System.out.println("Nomor Kamar\tKetersediaan");
-        for (int i = 0; i < informasiKamar.length; i++) {
-            System.out.println(informasiKamar[i][0] + "\t\t\t" + informasiKamar[i][1]);
-        }
-     
                 break;
-           
+
             case 3:
-            System.out.println("tes 3");
-            break;
-            case 4:
-            int MAX_TAMU = 100;
-            String[][] riwayatTamu = new String[MAX_TAMU][4];
-            String [] nama = {"ren" , "ron", "rin"}; 
-            String [] checkin = {"11-12-2023","12-12-2023","15-12-2023"};
-            String [] checkout = {"11-12-2023","12-12-2023","15-12-2023"};
-            int [] nomor_kamar = {1,2,3};
 
-
-            System.out.println("===RIWAYAT TAMU===");
-            for (int i = 0; i < nama.length; i++) { 
-                System.out.printf("%-3d| %-10s| %-20s| %-20s| %-3d | \n", (i+1), nama[i], checkin[i], checkout[i], nomor_kamar[i]);
-                    
-
-            }
-        
                 break;
-    }
+        
+            default:
+                break;
+        }
 }
 }
 }
+
 
 
         
